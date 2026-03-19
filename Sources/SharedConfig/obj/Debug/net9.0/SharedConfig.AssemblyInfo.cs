3be176b00e690c1b4e0c4bd3d8832a68fdaddbac
ChaariMahmoud <chaarimahmoud55@gmail.com>
2026-03-19 14:22:29 +0100

    Extracting Corral execution trace

--- Sources/SharedConfig/obj/Debug/net9.0/SharedConfig.AssemblyInfo.cs
+++ Sources/SharedConfig/obj/Debug/net9.0/SharedConfig.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedConfig")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10dcd5eca633c21d2f62193e78711596d61c07f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6775a4e26693b9e2c76eea15dd0a5787b710990")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedConfig")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedConfig")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
