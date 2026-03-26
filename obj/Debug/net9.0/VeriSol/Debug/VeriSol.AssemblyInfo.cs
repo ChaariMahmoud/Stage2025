7211d82341b8a0887c7ad0ba50a894c27818c2fe
ChaariMahmoud <chaarimahmoud55@gmail.com>
2026-03-26 16:51:23 +0100

    Clear Corral CounterExemple + Refactor v2

--- obj/Debug/net9.0/VeriSol/Debug/VeriSol.AssemblyInfo.cs
+++ obj/Debug/net9.0/VeriSol/Debug/VeriSol.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("A formal specification and verification tool for Solidity")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.1-alpha+e8ffb525d3b31651f8870b063a78c0a450d79de2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.1-alpha+9ee2fe34f4ad2f064de0c2c5b711b62b66ec6ac1")]
 [assembly: System.Reflection.AssemblyProductAttribute("VeriSol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VeriSol")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.1.1.0")]
