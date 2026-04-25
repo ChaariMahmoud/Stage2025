a2587063020e050b048c70c333287805aada5c4c
ChaariMahmoud <chaarimahmoud55@gmail.com>
2026-04-26 00:17:38 +0200

    First Benchmark attempt

--- Test/regressions/NoConstructor.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity >=0.4.24 <0.6.0;
-
-interface NoConstructor {
-    function activate() external;
-    function getAddress() external view returns (address);
-}
--- WasmInputs/newYear.wat
+++
@@ -1,6 +0,0 @@
-(module
-  (func $t1 (result f32)
-    (f32.const 3.7)
-    (f32.floor)
-  )
-)
